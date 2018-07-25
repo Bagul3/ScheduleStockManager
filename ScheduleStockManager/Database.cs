ae02742d1bdbc58588fd5109eeaa90d632f4309d
Bagul16 <vandershannon@gmail.com>
2018-07-25 21:41:30 +0100

    Updated

--- ScheduleStockManager/Database.cs
+++ ScheduleStockManager/Database.cs
@@ -2,11 +2,11 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Data.OleDb;
+using System.Diagnostics;
 using System.IO;
-using System.Runtime.CompilerServices;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
 using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 using ScheduleStockManager;
 using ScheduleStockManager.Mechanism;
 using ScheduleStockManager.Models;
@@ -16,6 +16,7 @@ namespace StockCSV
     public class Database : Job
     {
         private readonly LogWriter _logger = new LogWriter();
+        private List<string> doneList = new List<string>();
 
         public void CreateDbfFile(List<string> t2TreFs)
         {
@@ -43,71 +44,81 @@ namespace StockCSV
             }
         }
 
-        public override string DoJob(DataSet data)
+        public override void InsertIntoDescriptions(string sku)
+        {
+            Connection(sku, SqlQueries.InsertSKU);
+        }
+
+        public override string DoJob(DataRow dr, DataSet dt)
         {
             try
             {
-                if (data.Tables.Count == 0)
+                if (dr == null)
                     return "";
                 var csv = new StringBuilder();
                 var actualStock = "0";
                 var inStockFlag = false;
                 var groupSkus = "";
 
-                foreach (DataRow dr in data.Tables[0].Rows)
-                {
-                    _logger.LogWrite("Working....");
-                    var isStock = 0;
-                    for (var i = 1; i < 14; i++)
-                    {
-                        if (!string.IsNullOrEmpty(dr["QTY" + i].ToString()))
-                        {
-                            if (dr["QTY" + i].ToString() != "")
-                            {
-                                if (Convert.ToInt32(dr["QTY" + i]) > 0)
-                                {
-                                    if (String.IsNullOrEmpty(dr["LY" + i].ToString()))
-                                    {
-                                        actualStock = dr["QTY" + i].ToString();
-                                    }
-                                    else
-                                    {
-                                        actualStock =
-                                            (Convert.ToInt32(dr["QTY" + i]) - Convert.ToInt32(dr["LY" + i]))
-                                            .ToString();
-                                    }
+                _logger.LogWrite("Working....");
 
-                                    isStock = 1;
-                                    inStockFlag = true;
+                var isStock = 0;
+                for (var i = 1; i < 14; i++)
+                {
+                    if (!string.IsNullOrEmpty(dr["QTY" + i].ToString()))
+                    {
+                        if (dr["QTY" + i].ToString() != "")
+                        {
+                            if (Convert.ToInt32(dr["QTY" + i]) > 0)
+                            {
+                                if (dr["LY" + i].ToString() == "0" ||
+                                    string.IsNullOrEmpty(dr["LY" + i].ToString()))
+                                {
+                                    actualStock = dr["QTY" + i].ToString();
                                 }
                                 else
                                 {
-                                    isStock = 0;
+                                    actualStock =
+                                        (Convert.ToInt32(dr["QTY" + i]) - Convert.ToInt32(dr["LY" + i]))
+                                        .ToString();
                                 }
-                                var append = (1000 + i).ToString();
-                                groupSkus = dr["NewStyle"].ToString();
-                                var groupSkus2 = dr["NewStyle"] + append.Substring(1, 3);
-                                var newLine = $"{"\"" + groupSkus2 + "\""},{"\"" + actualStock + "\""},{"\"" + isStock + "\""}";
-                                csv.AppendLine(newLine);
-                            }
-                            actualStock = "0";
-                        }
-                    }
 
-                    isStock = inStockFlag ? 1 : 0;
-                    if (!string.IsNullOrEmpty(dr["NewStyle"].ToString()))
-                    {
-                        var newLine2 = $"{"\"" + groupSkus + "\""},{"\"" + actualStock + "\""},{"\"" + isStock + "\""}";
-                        csv.AppendLine(newLine2);
-                    }
-                    inStockFlag = false;
-                    if (data.Tables[0].Rows.Count > 1)
-                    {
-                        break;
+                                isStock = actualStock == "0" ? 0 : 1;
+
+                                inStockFlag = true;
+                            }
+                            else
+                            {
+                                isStock = 0;
+                            }
+                            var append = (1000 + i).ToString();
+                            groupSkus = dr["NewStyle"].ToString();
+                            var groupSkus2 = dr["NewStyle"] + append.Substring(1, 3);
+
+                            var eanRow = dt.Tables[0].Select("T2T_CODE = '" + groupSkus2 + "'").FirstOrDefault();
+                            var eanCode = "";
+                            if (eanRow != null)
+                            {
+                                eanCode = eanRow["EAN_CODE"].ToString();
+                            }
+                            var newLine = $"{"\"" + groupSkus2 + "\""},{"\"" + actualStock + "\""},{"\"" + isStock + "\""},{"\"" + Convert.ToDateTime(dr["LASTDELV"]).ToString("yyyy/MM/dd") + "\""},{"\"" + RemoveLineEndings(eanCode) + "\""}";
+                            csv.AppendLine(newLine);
+                            
+                        }
+                        actualStock = "0";
                     }
                 }
-                Console.WriteLine("Job Finished");
-                _logger.LogWrite("Finished");
+                doneList.Add(dr["NewStyle"].ToString());
+
+
+                isStock = inStockFlag ? 1 : 0;
+                if (!string.IsNullOrEmpty(dr["NewStyle"].ToString()))
+                {
+                    var empty = "";
+                    var newLine2 = $"{"\"" + groupSkus + "\""},{"\"" + actualStock + "\""},{"\"" + isStock + "\""},{"\"" + Convert.ToDateTime(dr["LASTDELV"]).ToString("yyyy/MM/dd") + "\""},{"\"" + empty + "\""}";
+                    csv.AppendLine(newLine2);
+                }
+
                 return csv.ToString();
             }
             catch (Exception e)
@@ -129,14 +140,18 @@ namespace StockCSV
             }
         }
 
-        public override DataSet Connection(string reff)
+        public override DataSet Connection(string reff, string query)
         {
             var dataset = new DataSet();
             using (var connectionHandler = new OleDbConnection(System.Configuration.ConfigurationManager.AppSettings["AccessConnectionString"]))
             {
                 connectionHandler.OpenAsync();
-                var myAccessCommand = new OleDbCommand(SqlQueries.StockQuery, connectionHandler);
-                myAccessCommand.Parameters.AddWithValue("?", reff);
+                var myAccessCommand = new OleDbCommand(query, connectionHandler);
+                if (reff != null)
+                {
+                    myAccessCommand.Parameters.AddWithValue("?", reff);
+                }
+
                 var myDataAdapter = new OleDbDataAdapter(myAccessCommand);
                 myDataAdapter.Fill(dataset);
             }
@@ -162,6 +177,23 @@ namespace StockCSV
         public override TimeSpan GetEndTime()
         {
             return TimeSpan.Parse(System.Configuration.ConfigurationManager.AppSettings["EndTime"]);
+        }
+
+        public static string RemoveLineEndings(string value)
+        {
+            if (String.IsNullOrEmpty(value))
+            {
+                return value;
+            }
+
+            string lineSeparator = ((char) 0x2028).ToString();
+            string paragraphSeparator = ((char) 0x2029).ToString();
+
+            return value.Replace("\r\n", string.Empty)
+                .Replace("\n", string.Empty)
+                .Replace("\r", string.Empty)
+                .Replace(lineSeparator, string.Empty)
+                .Replace(paragraphSeparator, string.Empty);
         }
     }
 }
--- ScheduleStockManager/Models/SqlQueries.cs
+++ ScheduleStockManager/Models/SqlQueries.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ScheduleStockManager.Models
+﻿namespace ScheduleStockManager.Models
 {
     public static class SqlQueries
     {
@@ -13,13 +7,19 @@ namespace ScheduleStockManager.Models
                 Sum(T2_BRA.Q13) AS QTY3, Sum(T2_BRA.Q14) AS QTY4, Sum(T2_BRA.Q15) AS QTY5, Sum(T2_BRA.Q16) AS QTY6, Sum(T2_BRA.Q17) AS QTY7, Sum(T2_BRA.Q18) AS QTY8, 
                     Sum(T2_BRA.Q19) AS QTY9, Sum(T2_BRA.Q20) AS QTY10, Sum(T2_BRA.Q21) AS QTY11, Sum(T2_BRA.Q22) AS QTY12, Sum(T2_BRA.Q23) AS QTY13, T2_HEAD.REF,
                         Sum(T2_BRA.LY11) AS LY1, Sum(T2_BRA.LY12) AS LY2, Sum(T2_BRA.LY13) AS LY3, Sum(T2_BRA.LY14) AS LY4, Sum(T2_BRA.LY15) AS LY5, 
-                            Sum(T2_BRA.LY16) AS LY6, Sum(T2_BRA.LY17) AS LY7, Sum(T2_BRA.LY18) AS LY8, Sum(T2_BRA.LY19) AS LY9, Sum(T2_BRA.LY20) AS LY10, 
-                                Sum(T2_BRA.LY21) AS LY11, Sum(T2_BRA.LY22) AS LY12, Sum(T2_BRA.LY23) AS LY13 
-									FROM (((T2_BRA INNER JOIN T2_HEAD ON T2_BRA.REF = T2_HEAD.REF) INNER JOIN 
-									(SELECT Right(T2_LOOK.[KEY],3) AS NewCol, T2_LOOK.F1 AS MasterColour, Left(T2_LOOK.[KEY],3) AS Col, T2_LOOK.F7
-								FROM T2_LOOK
-								WHERE (Left(T2_LOOK.[KEY],3))='COL') as Colour ON T2_BRA.COLOUR = Colour.NewCol))
-                                    WHERE [T2_BRA].[REF] = ?
-									GROUP BY ([T2_BRA].[REF] + [F7]),T2_HEAD.REF";
+                            Sum(T2_BRA.LY16) AS LY6, Sum(T2_BRA.LY17) AS LY7, Sum(T2_BRA.LY18) AS LY8, Sum(T2_BRA.LY19) AS LY9, Sum(T2_BRA.LY20) AS LY10,
+                                Sum(T2_BRA.LY21) AS LY11, Sum(T2_BRA.LY22) AS LY12, Sum(T2_BRA.LY23) AS LY13, T2_HEAD.LASTDELV
+									FROM ((((T2_BRA INNER JOIN T2_HEAD ON T2_BRA.REF = T2_HEAD.REF)) INNER JOIN 
+									(SELECT Right(t.[KEY],3) AS NewCol, t.F1 AS MasterColour, Left(t.[KEY],3) AS Col, t.F7 
+								FROM T2_LOOK t
+								WHERE (Left(t.[KEY],3))='COL') as Colour ON T2_BRA.COLOUR = Colour.NewCol) INNER JOIN [DESC] ON [T2_BRA].[REF] = [DESC].SKU)
+                                    WHERE T2_BRA.BRANCH in ('A','G')
+									GROUP BY ([T2_BRA].[REF] + [F7]),T2_HEAD.REF, T2_HEAD.LASTDELV";
+
+        public static string GetEanCodes => "SELECT * from T2_EAN WHERE EAN_CODE <> NULL";
+
+        public static string InsertSKU => @"INSERT INTO [DESC] (SKU) VALUES (?)";
+
+        public static string DeleteSKUs => @"DELETE FROM [DESC]";
     }
 }
